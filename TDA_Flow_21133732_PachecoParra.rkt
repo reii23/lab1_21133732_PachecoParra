0c6176cd61a50d33ce246f1a612dbb29073beb7f
Reinaldo Pacheco <reinaldo.pacheco@usach.cl>
2023-09-24 14:53:02 -0300

    add TDA_Flow
    se agrega el tda_flow y se modifica el tda_option

---
+++ TDA_Flow_21133732_PachecoParra.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+; TDA_Flow_21133732_PachecoParra
+
+(require "TDA_Option_21133732_PachecoParra.rkt")
+
+(provide(all-defined-out))
+
+; 3. TDA_Flow - constructor
+; nombre funcion: flow
+; Dom: id (int) X name-msg (String) X Option*
+; Recorrido: flow
+; Descripción: Función constructora de un flujo de un chatbot.
+; Requisitos: Usar estructuras basadas en listas y/o pares
+; Flujos quedan identificados por un ID único. Su unicidad se verifica al momento de agregarlo a un chatbot a fin de evitar duplicidad. La función también verifica que las opciones añadidas no se repitan en base al id de éstos.
+
+(define (flow id name-msg . options)
+  (list id name-msg (remove-duplicates-by-id options)))
+
+; Funcion para eliminar los id duplicados
+(define (remove-duplicates-by-id options)
+  (foldl 
+   (lambda (opt acc)
+     (if (option-exists? opt acc)
+         acc
+         (append acc (list opt))))
+   '()
+   options))
+
+; SELECTORES:
+(define (get-id flow)
+  (car flow))
+
+(define (get-name-msg flow)
+  (cadr flow))
+
+(define (get-options flow)
+  (caddr flow))
+
+; PERTENENCIA
+
+(define (option-exists? option options)
+  (not (null? (filter (lambda (opt) 
+                       (and (number? (get-id opt))
+                            (number? (get-id option))
+                            (= (get-id opt) (get-id option)))) options))))
--- TDA_Option_21133732_PachecoParra.rkt
+++ TDA_Option_21133732_PachecoParra.rkt
@@ -1,15 +1,42 @@
 #lang racket
 
+(provide(all-defined-out))
+        
 ; TDA_Option_21133732_PachecoParra
 
 ; --------------------------------------
 ; 2.
 ; TDA Option - constructor
-; nombre funcion: flow
-; Dominio: code (Int)  X message (String)  X ChatbotCodeLink (Int) X FlowCodeLink (Int) X Keyword*
+; nombre funcion: option
+; Dominio: code (Int)  X message (String)  X ChatbotCodeLink (Int) X InitialFlowCodeLink (Int) X Keyword*
 ; Recorrido: option
 ; Descripción: Función constructora de una opción para flujo de un chatbot. Cada opción se enlaza a un chatbot y flujo especificados por sus respectivos códigos.
 ; Requisitos de implementación: Usar estructuras basadas en listas y/o pares. El código de un option es único.  Su unicidad se verifica al momento de agregarlo a un flow a fin de evitar duplicidad.
 
-(define (option code message ChatbotCodeLink FlowCodeLink . keywords)
-  (list code message ChatbotCodeLink FlowCodeLink keywords))
+(define (option code message ChatbotCodeLink InitialFlowCodeLink . keywords)
+  (list code message ChatbotCodeLink InitialFlowCodeLink keywords))
+
+; Selectores:
+; ejemplos de uso:
+; (get-code op1)
+; (get-message op2)
+
+(define (get-code option)
+  (car option))
+
+(define (get-message option)
+  (cadr option))
+
+(define (get-chatbotcodelink option)
+  (cadr option))
+
+(define (get-initialflowcodelink option)
+  (cadddr option))
+
+(define (get-keywords option)
+  (car (cddddr option)))
+
+(define op1 (option  1 "1) Viajar" 1 1 "viajar" "turistear" "conocer")) ; verifica RFN 2
+
+(define op2 (option  2 "2) Estudiar" 2 1 "estudiar" "aprender" "perfeccionarme")) ; verifica RFN 2
+
