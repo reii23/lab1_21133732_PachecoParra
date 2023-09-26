d41de62ccf7fe6ccdda92795b79bcb3b5703422b
Reinaldo Pacheco <reinaldo.pacheco@usach.cl>
2023-09-26 07:41:15 -0300

    add TDA_Chatbot and RFN5
    se agrega el archivo TDA_Chatbot y las pruebas del RFN5 en el archivo de pruebas

---
+++ TDA_Chatbot_21133732_PachecoParra.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+
+; TDA_Chatbot_21133732_PachecoParra
+
+(provide(all-defined-out))
+
+(require "TDA_Flow_21133732_PachecoParra.rkt")
+(require "TDA_Option_21133732_PachecoParra.rkt")
+
+; 5. TDA Chatbot - constructor
+; nombre funcion: chatbot
+; Dom: chatbotID (int) X name (String) X welcomeMessage (String) X startFlowId (int)  X  flows*
+; Rec: chatbot
+; Desc: Funcion constructora de un chatbot
+
+(define (chatbot chatbotID name welcomeMessage startFlowId . flows)
+  (list chatbotID name welcomeMessage startFlowId (remove-duplicates flows)))
+
+; Función auxiliar para verificar si un flujo ya existe en una lista dada de flujos.
+(define (flow-exists? flow flows)
+  (not (null? (filter (lambda (fl) (= (get-id fl) (get-id flow))) flows)))) ; Compara los ID de los flujos.
+
+; Función auxiliar para eliminar flujos duplicados basándose en su ID.
+(define (remove-duplicates flows)
+  (foldl 
+   (lambda (flow acc) ; Toma un flujo y la lista acc
+     (if (flow-exists? flow acc) ; Si el flujo ya existe en la lista acc
+         acc ; devuelve la lista acc sin cambios
+         (append acc (list flow)))) ;sino añade el flujo a la lista acumulada.
+   '()
+   flows))
--- pruebas_21133732_PachecoParra.rkt
+++ pruebas_21133732_PachecoParra.rkt
@@ -2,6 +2,7 @@
 
 (require "TDA_Flow_21133732_PachecoParra.rkt")
 (require "TDA_Option_21133732_PachecoParra.rkt")
+(require "TDA_Chatbot_21133732_PachecoParra.rkt")
 
 ; PRUEBAS
 
@@ -12,3 +13,5 @@
 (define f10 (flow 1 "flujo1" op1 op2 op2 op2 op2 op1))
 
 (define f11 (flow-add-option f10 op1)) ;se intenta añadir opción duplicada verifica RFN 4
+
+(define cb0 (chatbot 0 "Inicial" "Bienvenido\n ¿Qué te gustaría hacer?" 1 f10 f10 f10 f10))  ;solo añade una ocurrencia de f10, verifica RFN 5
